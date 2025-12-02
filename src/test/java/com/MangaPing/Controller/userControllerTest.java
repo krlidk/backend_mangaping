863be559085709acd262650913267f4d3879f6cd
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2025-12-02 20:56:39 -0300

    configuracion de seguridad

--- src/test/java/com/MangaPing/Controller/userControllerTest.java
+++ src/test/java/com/MangaPing/Controller/userControllerTest.java
@@ -1,7 +1,13 @@
 package com.MangaPing.Controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -12,12 +18,7 @@ import com.MangaPing.Service.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(controllers = userController.class)
 public class userControllerTest {
 
@@ -35,34 +36,34 @@ public class userControllerTest {
 		mockMvc.perform(post("/api/v1/user/registerUser")
 				.contentType("application/json")
 				.accept("application/json")
-				.content("""  
-				{
-					
-				"username": "usuario1",
-				"password": "12345678",
-				"email": "usuario1@gmail.com"
-				}
-				""")).andExpect(status().isOk());
-	} 
+				.content("""
+						{
+
+						"username": "usuario1",
+						"password": "12345678",
+						"email": "usuario1@gmail.com"
+						}
+						""")).andExpect(status().isOk());
+	}
+
 	@Test
 	public void login() throws Exception {
 
-		HttpSession session = null;
-		String username = "petiso";
-		String password = "12345678";
-
-
-		when(userService.loginUser(username, password,session)).thenReturn(ResponseEntity.ok().build());
+		when(userService.loginUser(
+				any(String.class),
+				any(String.class),
+				any(HttpSession.class))).thenReturn(ResponseEntity.ok().build());
 
 		mockMvc.perform(post("/api/v1/user/auth/login")
 				.contentType("application/json")
 				.accept("application/json")
-				.content("""  
-				{
-					
-				"username": "usuario1",
-				"password": "12345678"
-				}
-				""")).andExpect(status().isOk());
-	} 
+				.content("""
+						{
+						    "username": "usuario1",
+						    "password": "12345678"
+						}
+						"""))
+				.andExpect(status().isOk());
+	}
+
 }
